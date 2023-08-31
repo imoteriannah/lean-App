9620b46b8368e1a5d508f8a402b68156bbab5947
imoteriannah <imoter.iannah@student.moringaschool.com>
2023-08-31 08:36:03 +0100

    fixed issue accessing db.json file due to error in url

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function fetchUser() {
   const entry = document.querySelector("#search_text");
-
+  //   http://localhost:3000/users
   return fetch("http://localhost:3000/users")
     .then(function (response) {
       return response.json();
@@ -16,12 +16,16 @@ function renderUser(user) {
     const userContainer = document.createElement("div");
     userContainer.classList.add("user_profile");
     const username = document.createElement("h2");
+    username.classList.add("user_profile_info");
     username.innerHTML = user.userName;
     const profession = document.createElement("h2");
+    profession.classList.add("user_profile_info");
     profession.innerHTML = user.userProfession;
     const location = document.createElement("h2");
+    location.classList.add("user_profile_info");
     location.innerHTML = user.userLocation;
     const age = document.createElement("h2");
+    age.classList.add("user_profile_info");
     age.innerHTML = user.userAge;
     content.appendChild(userContainer);
     userContainer.appendChild(username);
@@ -32,6 +36,7 @@ function renderUser(user) {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
+  // const data =
   const search = document.querySelector("#submit");
   search.addEventListener("click", fetchUser());
 });
